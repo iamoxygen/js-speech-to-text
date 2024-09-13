994a1006a9aa1d97465e6dd3189153162d36baef
iamoxygen <vikash.chaubey0.vc@gmail.com>
2024-09-13 19:35:28 +0530

    Added files

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const toggleBtn = document.getElementById("toggleBtn");
 const output = document.getElementById("output");
+const speakBtn = document.getElementById("speakBtn");
+const pauseNResume = document.getElementById("pausenresume");
 
 let recognition;
 let isListening = false;
-let finalTranscript = ""; // To store the entire transcript
+let finalTranscript = "";
+
+// for speaking
+const synth = window.speechSynthesis;
+let isSpeaking = false;
+let isPause = false;
 
 if ("webkitSpeechRecognition" in window) {
   recognition = new webkitSpeechRecognition();
@@ -70,14 +77,80 @@ function stopRecognition() {
   toggleBtn.textContent = "Start Listening";
 }
 
+function handelSpeak() {
+  isSpeaking = !isSpeaking;
+
+  if (isSpeaking) {
+    speak();
+  } else {
+    stopSpeak();
+  }
+}
+
+function handelPauseAndResume() {
+  try {
+    isPause = !isPause;
+
+
+    if (isPause) {
+      pauseSpeaker();
+    } else {
+      resumeSpeaker();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function pauseSpeaker() {
+  try {
+    synth.pause()
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function resumeSpeaker() {
+  try {
+    synth.resume()
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function stopSpeak() {
+  try {
+    isSpeaking = false;
+    speakBtn.classList.remove("bg-red-500", "hover:bg-red-600");
+    speakBtn.classList.add("bg-blue-500", "hover:bg-blue-600");
+    synth.cancel();
+    speakBtn.textContent = "Speak";
+    pauseNResume.classList.add("hidden");
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function speak() {
-  // Create a SpeechSynthesisUtterance
-  const utterance = new SpeechSynthesisUtterance(output.value);
+  try {
+    isSpeaking = true;
+    // Create a SpeechSynthesisUtterance
+    const utterance = new SpeechSynthesisUtterance(output.value);
+    utterance.lang = "en-US";
 
-  // Select a voice
-  const voices = speechSynthesis.getVoices();
-  utterance.voice = voices[0]; // Choose a specific voice
+    // Select a voice
+    const voices = synth.getVoices();
+    utterance.voice = voices[1]; // Choose a specific voice
 
-  // Speak the text
-  speechSynthesis.speak(utterance);
+    // Speak the text
+    speechSynthesis.speak(utterance);
+
+    pauseNResume.classList.remove("hidden");
+    speakBtn.classList.remove("bg-blue-500", "hover:bg-blue-600");
+    speakBtn.classList.add("bg-red-500", "hover:bg-red-600");
+    speakBtn.textContent = "Speaking...";
+  } catch (error) {
+    console.log(error);
+  }
 }
